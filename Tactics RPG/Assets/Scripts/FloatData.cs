72627098dede78cd2ec87db12b89547897955a03
Dane Ericksen <10721110@my.uvu.edu>
2021-08-30 11:31:32 -0600

    Refreshed on Scriptable Object in Unity

---
+++ Tactics RPG/Assets/Scripts/FloatData.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+    public float value;
+
+    public void AddToValue(float num)
+    {
+        value += num;
+    }
+}

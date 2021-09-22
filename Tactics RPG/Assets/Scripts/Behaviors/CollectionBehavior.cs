4dea93276cd8e43303ac9dce60f6589680ea7e27
Dane Ericksen <10721110@my.uvu.edu>
2021-09-22 11:40:17 -0600

    Went over using and accessing Collections in Class

--- Tactics RPG/Assets/Scripts/Behaviors/CollectionBehavior.cs
+++ Tactics RPG/Assets/Scripts/Behaviors/CollectionBehavior.cs
@@ -1,15 +1,47 @@
+using System;
 using UnityEngine;
 
 public class CollectionBehavior : MonoBehaviour
 {
+    private GameObject art;
+    private SpriteRenderer artSpriteRenderer;
     public CollectableSO collectedObj;
-    
-    // Start is called before the first frame update
-    void Start()
+    public CollectionsSO collection;
+
+    void Awake()
     {
-        if (collectedObj.collected)
+     ConfigCollectable();
+    }
+
+    public void SwapCollectable(CollectableSO collectable)
+    {
+        collectedObj = collectable;
+        ConfigCollectable();
+    }
+    
+    private void ConfigCollectable()
+    {
+        art = GetComponent<Transform>().gameObject;
+        artSpriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        if (artSpriteRenderer != null)
         {
-            Destroy(gameObject);
-        }   
+            //artSpriteRenderer.sprite = collectedObj.art2D;
+            //artSpriteRenderer.color = collectedObj.artColorTint;
+        }
+        EnableDisableCollectable(!collectedObj.collected);
+
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        collection.Collect(collectedObj);
+        EnableDisableCollectable(false);
+    }
+
+
+    private void EnableDisableCollectable(bool value)
+    {
+        //ArticulationBody.SetActive(value);
+        GetComponent<Collider>().enabled = value;
     }
 }

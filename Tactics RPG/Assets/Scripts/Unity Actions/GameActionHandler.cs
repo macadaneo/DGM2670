13eb0349a5307b92964ee5b0941c29623839a56f
Dane Ericksen <10721110@my.uvu.edu>
2021-10-06 11:18:54 -0600

    Learned about UnityActions

---
+++ Tactics RPG/Assets/Scripts/Unity Actions/GameActionHandler.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameActionHandler : MonoBehaviour
+{
+    public GameAction gameActionObj;
+    public UnityEvent respondEvent;
+    private void Start()
+    {
+        gameActionObj.raise += Respond;
+    }
+
+    private void Respond()
+    {
+        respondEvent.Invoke();
+    }
+
+}

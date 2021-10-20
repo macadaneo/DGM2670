3a6e9d9717c048b930e674e268fb090bb11d3770
Dane Ericksen <10721110@my.uvu.edu>
2021-10-19 23:57:07 -0600

    Made Further Progress on State Machine and started creating States

--- Tactics RPG/Assets/Scripts/State Machine/StateMachine.cs
+++ Tactics RPG/Assets/Scripts/State Machine/StateMachine.cs
@@ -1,18 +1,59 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class StateMachine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public virtual State CurrentState
     {
-        
+        get
+        {
+            return _currentState;
+        }
+        set
+        {
+            Transition(value);
+        }
     }
+        protected State _currentState;
+        protected bool _inTransition;
 
-    // Update is called once per frame
-    void Update()
-    {
+        public virtual T GetState<T>() where T : State
+        {
+            T target = GetComponent<T>();
+            if (target == null)
+            {
+                target = gameObject.AddComponent<T>();
+            }
+
+            return target;
+        }
+
+        public virtual void ChangeState<T>() where T : State
+        {
+            CurrentState = GetState<T>();
+        }
+
+        protected virtual void Transition(State value)
+        {
+            if (_currentState == value || _inTransition)
+            {
+                return;
+            }
+            _inTransition = true;
+
+            if (_currentState != null)
+            {
+                _currentState.Exit();
+            }
+            _currentState = value;
+
+            if (_currentState != null)
+            {
+                _currentState.Enter();
+            }
+            _inTransition = false;
+        }
         
-    }
+        
+    
 }

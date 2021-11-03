b5649281caf5a4238398ca5e909ffaac519108ee
Dane Ericksen <10721110@my.uvu.edu>
2021-11-02 23:33:03 -0600

    Worked further on movement algorithm and selected tile color change

--- Tactics RPG/Assets/Scripts/View Model Component/Board.cs
+++ Tactics RPG/Assets/Scripts/View Model Component/Board.cs
@@ -8,7 +8,17 @@ public class Board : MonoBehaviour
 {
     [SerializeField] private GameObject tilePrefab;
     public Dictionary<Point, Tile> tiles = new Dictionary<Point, Tile>();
-
+    private Color selectedTileColor = new Color(0, 1, 1, 1);
+    private Color defaultTileColor = new Color(1, 1, 1, 1);
+    
+    private Point[] dirs = new Point[4]
+    {
+        new Point(0, 1),
+        new Point(0, -1),
+        new Point(1, 0),
+        new Point(-1, 0)
+    };
+    
     public void Load (LevelData data)
     {
         for (int i = 0; i <data.tiles.Count; ++i)
@@ -24,8 +34,75 @@ public class Board : MonoBehaviour
     {
         List<Tile> retValue = new List<Tile>();
         retValue.Add(start);
-        // Add more code here
+        ClearSearch();
+        Queue<Tile> checkNext = new Queue<Tile>();
+        Queue<Tile> checkNow = new Queue<Tile>();
+        
+        while (checkNow.Count > 0)
+        {
+            Tile t = checkNow.Dequeue();
+            
+            for (int i = 0; i < 4; i++)
+            {
+                Tile next = GetTile(t.pos + dirs[i]);
+
+                if (next == null || next.distance <= t.distance +1)
+                {
+                    continue;
+                }
+
+                if (addTile(t, next))
+                {
+                    next.distance = t.distance + 1;
+                    next.prev = t;
+                    checkNext.Enqueue(next);
+                    retValue.Add(next);
+                }
+            }
+
+            if (checkNow.Count == 0)
+            {
+                SwapReference(ref checkNow, ref checkNext);
+            }
+
+        }
         return retValue;
+    }
+
+    void SwapReference(ref Queue<Tile> a, ref Queue<Tile> b)
+    {
+        Queue<Tile> temp = a;
+        a = b;
+        b = temp;
+    }
+    public Tile GetTile(Point p)
+    {
+        return tiles.ContainsKey(p) ? tiles[p] : null;
+    }
+
+    void ClearSearch()
+    {
+        foreach (Tile t in tiles.Values)
+        {
+            t.prev = null;
+            t.distance = int.MaxValue;
+        }
+    }
+
+    public void SelectTiles(List<Tile> tiles)
+    {
+        for (int i = tiles.Count -1; i >= 0; i++)
+        {
+            tiles[i].GetComponent<Renderer>().material.SetColor("_Color", selectedTileColor);
+        }
+    }
+
+    public void DeSelectTiles(List<Tile> tiles)
+    {
+        for (int i = tiles.Count -1; i >= 0; --i)
+        {
+            tiles[i].GetComponent<Renderer>().material.SetColor("_Color", defaultTileColor);
+        }
     }
 }
 

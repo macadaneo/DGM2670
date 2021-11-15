2bf17d6c046beae162ac73c336e54442bfc3d948
Dane Ericksen <10721110@my.uvu.edu>
2021-11-14 23:27:58 -0700

    Now Spawning Units on Board

--- Tactics RPG/Assets/Scripts/Battle States/InitBattleState.cs
+++ Tactics RPG/Assets/Scripts/Battle States/InitBattleState.cs
@@ -14,8 +14,28 @@ public class InitBattleState : BattleState
          board.Load( levelData );
          Point p = new Point((int)levelData.tiles[0].x, (int)levelData.tiles[0].z);
          SelectTile(p);
+         SpawnTestUnits(); //This is new
          yield return null;
-         owner.ChangeState<MoveTargetState>();
+         owner.ChangeState<MoveTargetState>(); // This is changed
+    }
+
+    void SpawnTestUnits()
+    {
+        System.Type[] components = new System.Type[] { typeof(WalkMovement) };
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject instance = Instantiate(owner.heroPrefab) as GameObject;
+
+            Point p = new Point((int)levelData.tiles[i].x, (int)levelData.tiles[i].z);
+
+            Unit unit = instance.GetComponent<Unit>();
+            unit.Place((board.GetTile(p)));
+            unit.Match();
+
+            Movement m = instance.AddComponent(components[i]) as Movement;
+            m.range = 5;
+            m.jumpHeight = 1;
+        }
     }
 }
 

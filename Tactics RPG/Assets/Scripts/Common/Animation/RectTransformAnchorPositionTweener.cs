355e02e79afa718b8edb1305a75b619170f8b506
Dane Ericksen <10721110@my.uvu.edu>
2021-12-13 07:29:12 -0700

    Worked on UI
    Make the framework for Panels to pop up in game

---
+++ Tactics RPG/Assets/Scripts/Common/Animation/RectTransformAnchorPositionTweener.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using System.Collections;
+
+public class RectTransformAnchorPositionTweener : Vector3Tweener 
+{
+    RectTransform rt;
+	
+    void Awake ()
+    {
+        rt = transform as RectTransform;
+    }
+
+    protected override void OnUpdate ()
+    {
+        base.OnUpdate ();
+        rt.anchoredPosition = currentTweenValue;
+    }
+}

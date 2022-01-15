def0164e912e727312535dfde4842b023e1e5dd2
Dane Ericksen <10721110@my.uvu.edu>
2022-01-14 23:56:20 -0700

    Started on Conversation Controller

---
+++ Tactics RPG/Assets/Scripts/Controller/ConversationController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ConversationController : MonoBehaviour
+{
+    //[SerializeField] ConversationPanel leftPanel;
+    //[SerializeField] ConversationPanel rightPanel;
+
+    private Canvas _canvas;
+
+    private IEnumerator conversation;
+
+    private Tweener transition;
+
+}

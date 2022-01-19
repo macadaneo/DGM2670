a51c3704f30c32b876073bf3894dfd6a191c2e36
Dane Ericksen <10721110@my.uvu.edu>
2022-01-18 23:56:38 -0700

    Made Conversation Controller

--- Tactics RPG/Assets/Scripts/Controller/ConversationController.cs
+++ Tactics RPG/Assets/Scripts/Controller/ConversationController.cs
@@ -1,17 +1,98 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
 public class ConversationController : MonoBehaviour
 {
-    //[SerializeField] ConversationPanel leftPanel;
-    //[SerializeField] ConversationPanel rightPanel;
+    [SerializeField] ConversationPanel leftPanel;
+    [SerializeField] ConversationPanel rightPanel;
 
-    private Canvas _canvas;
-
+    private Canvas canvas;
     private IEnumerator conversation;
-
     private Tweener transition;
+
+    private const string ShowTop = "Show Top";
+    private const string ShowBottom = "Show Bottom";
+    private const string HideTop = "Hide Top";
+    private const string HideBottom = "Hide Bottom";
+
+    void Start()
+    {
+        canvas = GetComponentInChildren<Canvas>();
+        if (leftPanel.panel.CurrentPosition == null)
+        {
+            leftPanel.panel.SetPosition(HideBottom, false);
+        }
+
+        if (rightPanel.panel.CurrentPosition == null)
+        {
+            rightPanel.panel.SetPosition(HideBottom, false);
+        }
+        canvas.gameObject.SetActive(false);
+    }
+
+    public static event EventHandler completeEvent;
+
+    public void Show(ConversationData data)
+    {
+        canvas.gameObject.SetActive(true);
+        conversation = Sequence (data);
+        conversation.MoveNext();
+    }
+
+    public void Next()
+    {
+        if (conversation == null || transition != null)
+        {
+            return;
+        }
+        conversation.MoveNext();
+    }
+
+    IEnumerator Sequence (ConversationData data)
+    {
+        for (int i = 0; i < data.list.Count; ++i)
+        {
+            SpeakerData sd = data.list[i];
+            ConversationPanel currentPanel = (sd.anchor == TextAnchor.UpperLeft || sd.anchor == TextAnchor.MiddleLeft || sd.anchor == TextAnchor.LowerLeft) ? leftPanel : rightPanel;
+            IEnumerator presenter = currentPanel.Display(sd);
+            presenter.MoveNext();
+            string show, hide;
+            if (sd.anchor == TextAnchor.UpperLeft || sd.anchor == TextAnchor.UpperCenter || sd.anchor == TextAnchor.UpperRight)
+            {
+                show = ShowTop;
+                hide = HideTop;
+            }
+            else
+            {
+                show = ShowBottom;
+                hide = HideBottom;
+            }
+            currentPanel.panel.SetPosition(hide, false);
+            MovePanel(currentPanel, show);
+            yield return null;
+            while (presenter.MoveNext())
+                yield return null;
+            MovePanel(currentPanel, hide);
+            transition.completedEvent += delegate(object sender, EventArgs e) {
+                conversation.MoveNext();
+            };
+            yield return null;
+        }
+        canvas.gameObject.SetActive(false);
+        if (completeEvent != null)
+            completeEvent(this, EventArgs.Empty);
+    }
+
+    void MovePanel(ConversationPanel obj, string pos)
+    {
+        transition = obj.panel.SetPosition(pos, true);
+        transition.duration = 0.5f;
+        transition.equation = EasingEquations.EaseOutQuad;
+    }
+
 
 }
